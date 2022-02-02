7580eadea16092c0d34185046304cc59e24ccc03
Ryan O'Neill <ryanponeill00@gmail.com>
2022-02-02 08:09:43 -0500

    Completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    function eyesFocus(e) {
+        console.log("Good!");
+    }
+    function eyesBlur(e) {
+        console.log("Hey! Eyes on me!");
+    }
+    return <button onFocus={eyesFocus} onBlur={eyesBlur}>
+        Eyes on me</button>;
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function Keypad() {
+    function passHandler(event) {
+        console.log("Entering password...")
+    }
+    return <input type="password" onChange={passHandler}></input>;
+}
+
+
+export default Keypad;
